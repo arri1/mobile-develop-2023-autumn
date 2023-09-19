fd423120f2e6757a12173b700dadc07370e1e433
DANIIkprog <72724550+DANIIkprog@users.noreply.github.com>
2023-09-19 14:40:40 +0900

    123

--- ivanovDaniil/App.js
+++ ivanovDaniil/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Ivanov Daniil </Text>
+      <Text>123</Text>
       <StatusBar style="auto" />
     </View>
   );
