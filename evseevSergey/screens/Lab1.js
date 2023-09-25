eb96803fa4ac7017335326f8240f774df487dc5e
djwbhr <sergey.evseev62@gmail.com>
2023-09-25 16:41:03 +0900

    secondsCommit

---
+++ evseevSergey/screens/Lab1.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Lab1 = ()=>{
+    return(
+        <View>
+            <Text>Йоу</Text>
+        </View>
+    )
+}
+export default Lab1
---
+++ evseevSergey/screens/Lab2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Lab2 = ()=>{
+    return(
+        <View>
+            <Text>Не йоу</Text>
+        </View>
+    )
+}
+export default Lab2
