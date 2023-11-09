25ba54de2ad88896c37d2756149534de180698d2
DANIIkprog <72724550+DANIIkprog@users.noreply.github.com>
2023-11-09 19:57:48 +0900

    useEffect

--- ivanovDaniil/components/2LAB/Screen2.js
+++ ivanovDaniil/components/2LAB/Screen2.js
@@ -1,15 +1,43 @@
-import React from "react";
-import { Text,View,StyleSheet} from "react-native";
-
+import React, { useEffect, useState } from "react";
+import {
+	Text,
+	View,
+	StyleSheet,
+	TouchableHighlight,
+	FlatList,
+	Button,
+} from "react-native";
 
 export default function Screen2() {
-    return (
-        <View>
-            <Text>2 LAB</Text>
-        </View>
-    );
-  }
+	const [data, setData] = useState([]);
+
+	getAPIdata = async () => {
+		const url = "https://jsonplaceholder.typicode.com/users";
+		let result = await fetch(url);
+		result = await result.json();
+		setData(result);
+	};
+	useEffect(() => {
+		getAPIdata();
+	});
+
+	return (
+		<View>
+			<Text>API CALL</Text>
+			<View>
+				<FlatList
+					data={data}
+					renderItem={({ item }) => (
+						<View>
+							<Text>№{item.id}.  {item.name}</Text>
+						</View>
+					)}
+				/>
+			</View>
+		</View>
+	);
+}
 
 const styles = StyleSheet.create({
-  
+	but: {},
 });
