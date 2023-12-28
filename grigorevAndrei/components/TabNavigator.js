77f8445e96d8f758dc4f738c55b489fe048a3b94
tamaoolol <tamaoolol@gmail.com>
2023-12-28 21:45:21 +0900

    lab5fixedfix

--- grigorevAndrei/components/TabNavigator.js
+++ grigorevAndrei/components/TabNavigator.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { useSelector } from "react-redux";
 import Ionicons from "react-native-vector-icons/Ionicons";
-import SignOut from "./SignOut";
+import lab5 from "./lab5";
 
 const Tab = createBottomTabNavigator();
 
@@ -17,41 +17,15 @@ const TabNavigator = () => {
 
           if (route.name === "Lab1") {
             iconName = focused
-              ? "ios-information-circle"
-              : "ios-information-circle-outline";
-          } else if (route.name === "lab2") {
-            iconName = focused
-              ? "ios-information-circle"
-              : "ios-information-circle-outline";
-          } else if (route.name == "lab3") {
-            iconName = focused ? "bug" : "bug-outline";
           } else if (route.name === "lab5") {
             iconName = focused ? "ios-pluscircleo" : "ios-pluscircle";
-          } else if (route.name === "lab4") {
-            iconName = focused ? "ios-pluscircleo" : "ios-pluscircle";
-          } else if (route.name == "Выйти") {
-            iconName = focused
-              ? "ios-information-circle"
-              : "ios-information-circle-outline";
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
       })}
     >
-      <Tab.Screen
-        name="Lab1"
-        component={lab1}
-        options={{ tabBarBadge: counter > 0 ? counter : null }}
-      />
-      <Tab.Screen name="lab2" component={lab2} />
-      <Tab.Screen name="lab3" component={lab3} />
       <Tab.Screen name="lab5" component={lab5} />
-      <Tab.Screen
-        name="Выйти"
-        component={SignOut}
-        options={{ tabBarLabel: "Выйти" }}
-      />
     </Tab.Navigator>
   );
 };
