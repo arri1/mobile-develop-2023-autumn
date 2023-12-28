c39de5dd92d56d2559e3b9f02cba96cc860d3200
DANIIkprog <72724550+DANIIkprog@users.noreply.github.com>
2023-12-28 21:00:12 +0900

    123

--- ivanovDaniil/App.js
+++ ivanovDaniil/App.js
@@ -3,28 +3,21 @@ import { Text, View, StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Screen1 from "./components/1LAB/Screen1";
-import Screen2 from "./components/2LAB/Screen2";
+// import Screen2 from "./components/2LAB/Screen2";
 import { useState } from "react";
-
 
 const Tab = createBottomTabNavigator();
 export default function App() {
-  return (
-    <NavigationContainer >
-      <Tab.Navigator  >
-        <Tab.Screen
-          name="1lab"
-          component={Screen1}
-          
-        />
-
-        <Tab.Screen name="2lab" component={Screen2} />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Tab.Navigator>
+				<Tab.Screen name="1lab" component={Screen1} />
+			</Tab.Navigator>
+		</NavigationContainer>
+	);
 }
 const styles = StyleSheet.create({
-  navigator: {
-    backgroundColorcolor: "#4f7ec4"
-  }
+	navigator: {
+		backgroundColorcolor: "#4f7ec4",
+	},
 });
