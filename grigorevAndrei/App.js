82ac41eb91bad61c810145befd78abba814d0122
tamaoolol <tamaoolol@gmail.com>
2023-12-28 22:17:54 +0900

    lab2zxc

--- grigorevAndrei/App.js
+++ grigorevAndrei/App.js
@@ -1,13 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
 import store from "./store";
-import StackNavigation from "./components/StackNavigation";
+import TabNavigator from "./components/TabNavigator";
 
 const App = () => {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <StackNavigation />
+        <TabNavigator />
       </NavigationContainer>
     </Provider>
   );
--- grigorevAndrei/components/TabNavigator.js
+++ grigorevAndrei/components/TabNavigator.js
@@ -4,9 +4,6 @@ import { useSelector } from "react-redux";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import lab1 from "./lab1";
 import lab2 from "./lab2";
-import lab3 from "./lab3";
-import lab5 from "./lab5";
-import SignOut from "./SignOut";
 
 const Tab = createBottomTabNavigator();
 
@@ -49,13 +46,6 @@ const TabNavigator = () => {
         options={{ tabBarBadge: counter > 0 ? counter : null }}
       />
       <Tab.Screen name="lab2" component={lab2} />
-      <Tab.Screen name="lab3" component={lab3} />
-      <Tab.Screen name="lab5" component={lab5} />
-      <Tab.Screen
-        name="Выйти"
-        component={SignOut}
-        options={{ tabBarLabel: "Выйти" }}
-      />
     </Tab.Navigator>
   );
 };
