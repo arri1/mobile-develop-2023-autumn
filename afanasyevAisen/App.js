df3b1ccc2f740457f8bb906b1797c9450fe8d958
straitz <afanasiev.aysen@gmail.com>
2023-12-28 22:53:03 +0900

    Lab3

--- afanasyevAisen/App.js
+++ afanasyevAisen/App.js
@@ -4,7 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Lab1 from './Lab1';
 import Lab2 from './Lab2';
-
+import Lab3 from './Lab3';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,6 +14,7 @@ function App() {
       <Tab.Navigator>
         <Tab.Screen name="Lab1" component={Lab1} />
         <Tab.Screen name="Lab2" component={Lab2} />
+        <Tab.Screen name="Lab3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
