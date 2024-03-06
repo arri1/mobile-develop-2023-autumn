2983eee791a511a157281ae80885642c3c1117cc
Люба Данилова <73094030+biziland@users.noreply.github.com>
2024-03-07 00:44:32 +0900

    ш пукккув

--- danilovaLuba/App.js
+++ danilovaLuba/App.js
@@ -4,18 +4,44 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./HomeScreen";
 import FriendsScreen from "./FriendsScreen";
+import Icon from 'react-native-vector-icons/AntDesign'
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+const screenOptions = (route, color) => {
+  let iconName;
+
+  switch (route.name) {
+    case 'Home':
+      iconName = 'home';
+      break;
+    case 'Friends':
+      iconName = 'folder1';
+      break;
+    default:
+      break;
+  }
+
+  return <Icon name={iconName} color={color} size={24} />;
+};
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarIcon: ({color}) => screenOptions(route, color),
+      })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Friends" component={FriendsScreen} />
+    </Tab.Navigator>
+  );
+};
 
 // Определите вашу навигацию в компоненте App
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Friends" component={FriendsScreen} />
-      </Tab.Navigator>
+      <TabNavigator/>
     </NavigationContainer>
   );
 }
