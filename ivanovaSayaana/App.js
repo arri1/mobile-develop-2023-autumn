a7b37fefb25df09a2f6a7b90b3bba9b6c851230b
ivanovaSayaana <isayaana1212@gmail.com>
2024-05-30 20:21:20 +0900

    lab2

--- ivanovaSayaana/App.js
+++ ivanovaSayaana/App.js
@@ -1,15 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  StyleSheet,
+  Button
+} from "react-native";
 
 const App = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [editIndex, setEditIndex] = useState(-1);
+
+  const handleAddTask = () => {
+    if (task) {
+      if (editIndex !== -1) {
+        // Edit existing task
+        const updatedTasks = [...tasks];
+        updatedTasks[editIndex] = task;
+        setTasks(updatedTasks);
+        setEditIndex(-1);
+      } else {
+        // Add new task
+        setTasks([...tasks, task]);
+      }
+      setTask("");
+    }
+  };
+
+  const handleEditTask = (index) => {
+    const taskToEdit = tasks[index];
+    setTask(taskToEdit);
+    setEditIndex(index);
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  const renderItem = ({ item, index }) => (
+    <View style={styles.task}>
+      <Text style={styles.itemList}>{item}</Text>
+      <View style={styles.taskButtons}>
+        <TouchableOpacity onPress={() => handleEditTask(index)}>
+          <Text style={styles.editButton}>Поменять</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => handleDeleteTask(index)}>
+          <Text style={styles.deleteButton}>Удалить</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    // Обновляем заголовок при изменении счетчика
-    // В React Native мы можем использовать функцию заголовка навигации, если используем React Navigation
-    // Например:
-    // navigation.setOptions({ title: Count: ${count} });
-  }, [count]); // useEffect будет запускаться только при изменении count
+  useEffect(() => {}, [count]);
 
   const increment = () => {
     setCount(count + 1);
@@ -21,21 +70,95 @@ const App = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Счетчик</Text>
-      <Text >Счет: {count}</Text>
-      <Button title="Плюс 1" onPress={increment} />
-      <Button title="Минус 1" onPress={decrement} />
+      <Text style={styles.title}></Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Введите задачу"
+        value={task}
+        onChangeText={(text) => setTask(text)}
+      />
+      <TouchableOpacity style={styles.addButton} onPress={handleAddTask}>
+        <Text style={styles.addButtonText}>
+          {editIndex !== -1 ? "Обновить задачу" : "Добавить задачу"}
+        </Text>
+      </TouchableOpacity>
+      <FlatList
+        data={tasks}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+
+      <View>
+        <Text>Счетчик</Text>
+        <Text>Счет: {count}</Text>
+        <Button title="Плюс 1" onPress={increment} />
+        <Button title="Минус 1" onPress={decrement} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-      justifyContent: "center",
-    }
-  });
+  container: {
+    flex: 1,
+    padding: 40,
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    marginBottom: 7,
+    color: "green",
+  },
+  input: {
+    borderWidth: 3,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 18,
+  },
+  addButton: {
+    backgroundColor: "green",
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  addButtonText: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+  task: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 15,
+    fontSize: 18,
+  },
+  itemList: {
+    fontSize: 19,
+  },
+  taskButtons: {
+    flexDirection: "row",
+  },
+  editButton: {
+    marginRight: 10,
+    color: "green",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  deleteButton: {
+    color: "red",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
 
 export default App;
