04419636651372c532544da867ef09aa64148c43
Aina <ainaturantaeva240@gmail.com>
2024-06-01 23:07:56 +0900

    lab1

--- turantaevaAina/App.js
+++ turantaevaAina/App.js
@@ -2,8 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { StyleSheet, Text, View, Image, AppRegistry } from "react-native";
 import lab1 from "./components/lab1";
-import lab2 from "./components/lab2";
-import lab3 from "./components/lab3";
+
 
 const Tab = createBottomTabNavigator();
 
@@ -19,34 +18,6 @@ export default function App() {
               <View>
                 <Image
                   source={require("./icons/keys.png")}
-                  style={styles.img}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="lab2"
-          component={lab2}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Image
-                  source={require("./icons/tdl.png")}
-                  style={styles.img}
-                />
-              </View>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="lab3"
-          component={lab3}
-          options={{
-            tabBarIcon: ({ focused }) => (
-              <View>
-                <Image
-                  source={require("./icons/to-do-list.png")}
                   style={styles.img}
                 />
               </View>
