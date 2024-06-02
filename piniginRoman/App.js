f2ec7fc331c23973419aabf215293a2719a588b8
Flashka0531 <r_pinigin@mail.ru>
2024-06-02 14:41:24 +0900

    lab3

--- piniginRoman/App.js
+++ piniginRoman/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import lab1 from './components/lab1';
 import lab2 from './components/lab2';
+import lab3 from './components/lab3';
 
 
 const Tab = createBottomTabNavigator();
@@ -13,6 +14,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Counter" component={lab1} />
         <Tab.Screen name="ToDoList" component={lab2} /> 
+        <Tab.Screen name="EvenOdd" component={lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
